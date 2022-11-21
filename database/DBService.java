307b608ac6b98cb6a34c5a0922e24a951f724efc
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2022-11-21 00:15:30 -0300

    Changes by Administrador

--- database/DBService.java
+++ database/DBService.java
@@ -1,6 +1,10 @@
 package database;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBService {
     private static final String url = "jdbc:postgresql://192.168.56.4:5432/postgres";
@@ -42,5 +46,41 @@ public class DBService {
             System.out.println(ex.getMessage());
         }
         return id;
+    }
+
+    public static void setImagen(ArrayList<Integer> numeros, ArrayList<String> paises, ArrayList<String> paths)throws SQLException {
+        try{
+
+            int cantMax = numeros.size();
+            String query = "insert into figurita values (?, ?, ?)";
+            for (int j = 0; j < cantMax - 1; j++){
+                query += ", (?, ?, ?)";
+            }
+
+            ArrayList<FileInputStream> streams = new ArrayList<>();
+
+            PreparedStatement p = connect().prepareStatement(query);
+            for(int i = 0; i < cantMax; i++){
+                String path = paths.get(i);
+                String pais = paises.get(i);
+                int numero = numeros.get(i);
+                File f = new File(path);
+                FileInputStream s = new FileInputStream(f);
+                streams.add(s);
+
+                p.setInt(1 + i * 3, numero);
+                p.setString(2 + i * 3, pais);
+                p.setBinaryStream(3 + i * 3, s, (int)f.length());
+            }
+            p.executeUpdate();
+            for (FileInputStream stream : streams) {
+                stream.close();
+            }
+            System.out.println("Se termino de ingresar las figuritas");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
